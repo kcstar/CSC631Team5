8a71e5cade18de76a14322e953f0e4f7133c5a09
Kyle Chen <kylec641@gmail.com>
2022-04-08 01:48:30 -0700

    Testing out procedural generation with apartment buildings

--- TermProject/EndlessRunnerGame/Assets/Scripts/LevelGenerator.cs
+++ TermProject/EndlessRunnerGame/Assets/Scripts/LevelGenerator.cs
@@ -7,13 +7,13 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private Transform[] levels;
     [SerializeField] private Transform level_start;
     private Vector3 lastEndPosition;
-    private const float PLAYER_DISTANCE_SPAWN_LEVEL = 15f;
+    private const float PLAYER_DISTANCE_SPAWN_LEVEL = 30f;
 
 
     private void Awake()
     {
         lastEndPosition = level_start.Find("EndPosition").position;
-        spawnLevelPart();
+        //spawnLevelPart();
     }
 
     private void Update()
@@ -32,7 +32,8 @@ public class LevelGenerator : MonoBehaviour
     private Transform spawnLevel(Vector3 spawnPosition)
     {
         int selectedLevel = Random.Range(0, levels.Length);
-        Transform levelPartTransform = Instantiate(levels[selectedLevel], spawnPosition, Quaternion.identity);
+        //Transform levelPartTransform = Instantiate(levels[selectedLevel], spawnPosition, Quaternion.identity);
+        Transform levelPartTransform = Instantiate(levels[selectedLevel], spawnPosition + (new Vector3(36, 0, 0)), Quaternion.identity);
 
         Debug.Log($"Spawning level {selectedLevel} prefab...");
         return levelPartTransform;
